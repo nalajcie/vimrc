910aea7b1cbc1fcb758c4078ba643f43944a7029
Marek Białowąs <marek.bialowas@phoesys.com>
2017-08-11 12:22:02 +0200

    update plugins

--- sources_non_forked/vim-go/scripts/test.sh
+++ sources_non_forked/vim-go/scripts/test.sh
@@ -4,6 +4,10 @@ set -e
 
 cd $(dirname $0)
 
+# install dependencies
+go get github.com/fatih/gomodifytags
+go get golang.org/x/tools/cmd/goimports
+
 # cleanup test.log
 if [ -f "test.log" ]; then
    rm test.log
@@ -28,3 +32,14 @@ if [ -f "FAILED" ]; then
   exit 1
 fi
 echo 2>&1 "PASS"
+
+# Run vimhelplint
+[ -d vim-vimhelplint ] || git clone https://github.com/machakann/vim-vimhelplint
+echo "Running vimhelplint"
+lint=$(vim -esN --cmd 'set rtp+=./vim-vimhelplint' -c 'filetype plugin on' \
+        -c 'e ../doc/vim-go.txt' -c 'verb VimhelpLintEcho' -c q 2>&1)
+if [ -n "$lint" ]; then 
+       exit 1
+else
+       exit 0
+fi
